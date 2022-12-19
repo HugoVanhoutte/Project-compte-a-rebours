fe1ca2dbb7f0c6a1cf90b23925035bcda15c7332
HugoVanhoutte <116795821+HugoVanhoutte@users.noreply.github.com>
2022-12-19 13:12:55 +0100

    Impossible d'arreter le compte a rebours a 0 0 0

---
+++ app.js
@@ -0,0 +1,51 @@
+
+function Countdown(seconds, minutes,hours) {
+    this.seconds = seconds;
+    this.minutes = minutes;
+    this.hours = hours;
+}
+
+Countdown.prototype.count = function () {
+    let runningSeconds = this.seconds;
+    let runningMinutes = this.minutes;
+    let runningHours = this.hours;
+
+    let running = setInterval(() => {
+        if(runningSeconds === 0){
+
+            if(runningMinutes === 0){
+
+                if(runningHours === 0){
+
+
+                    clearInterval(running);
+                    console.log("stop");
+
+
+                } else {
+                    runningMinutes = 59;
+                    runningSeconds = 59;
+                    --runningHours;
+                }
+
+            } else {
+                runningSeconds = 59;
+                --runningMinutes;
+            }
+
+        } else {
+            --runningSeconds;
+        }
+
+        console.log(`${runningSeconds} ${runningMinutes} ${runningHours}`);
+    }, 1000);
+};
+
+testS = prompt("Entrez Secondes");
+testM = prompt("Entrez Minutes");
+testH = prompt("Entrez Heures");
+
+test = new Countdown(testS, testM, testH);
+test.count();
+
+//TODO HTML/CSS, Boutons
